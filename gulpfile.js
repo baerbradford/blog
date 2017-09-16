d3543b20331565e4dc9e5758de8e1b1b4dbba3d9
Baer <baer.bradford@gmail.com>
2017-09-16 16:14:28 -0500

    handlebars and markdown to generate a single page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 var gulp = require('gulp');
+var handlebars = require('Handlebars');
 var markdown = require('gulp-markdown');
+var tap = require('gulp-tap');
 
-gulp.task('default', function() {
-    return gulp.src('index.md')
-        .pipe(markdown())
-        .pipe(gulp.dest('docs'));
+gulp.task('clean', [], function() {
+
+});
+
+gulp.task('default', ['clean', 'generate-pages'], function() {
+    
+});
+
+gulp.task('generate-pages', [], function() {
+    return gulp.src('templates/main-layout.hbs')
+        .pipe(tap(function(file) {
+            var template = handlebars.compile(file.contents.toString());
+            return gulp.src('content/**.md')
+            .pipe(markdown())
+            .pipe(tap(function(file) {
+                var data = {
+                    content: file.contents.toString()
+                };
+                var html = template(data);
+                file.contents = new Buffer(html, 'utf-8');
+            }))
+            .pipe(gulp.dest('docs'));
+        }));
 });
